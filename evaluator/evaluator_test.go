b187724ba4b6b57efc19642440c5549fb749ab04
Angshuman Halder <angshuman.halder243@gmail.com>
2024-02-24 18:14:17 +0530

    Add builtin functions for array

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -443,21 +443,52 @@ func TestBuiltInFunctions(t *testing.T) {
 		{11, `len("hello world")`},
 		{"argument to `len` not supported, got INTEGER", `len(1)`},
 		{"wrong number of arguments. got=2, want=1", `len("one", "two")`},
+		{3, `len([1, 2, 3])`},
+		{0, `len([])`},
+		{1, `first([1, 2, 3])`},
+		{nil, `first([])`},
+		{"argument to `first` must be an ARRAY, got INTEGER", `first(1)`},
+		{3, `last([1, 2, 3])`},
+		{"argument to `last` must be an ARRAY, got INTEGER", `last(1)`},
+		{nil, `last([])`},
+		{[]int{2, 3}, `rest([1, 2, 3])`},
+		{nil, `rest([])`},
+		{[]int{1}, `push([], 1)`},
+		{"argument to `push` must be an ARRAY, got INTEGER", `push(1, 1)`},
 	}
 
 	for _, tt := range tests {
-		evaluted := testEval(tt.input)
+		evaluated := testEval(tt.input)
+
 		switch expected := tt.expected.(type) {
 		case int:
-			testIntegerObject(t, evaluted, int64(expected))
+			testIntegerObject(t, evaluated, int64(expected))
+		case nil:
+			testNullObject(t, evaluated)
 		case string:
-			errObj, ok := evaluted.(*object.Error)
+			errObj, ok := evaluated.(*object.Error)
 			if !ok {
-				t.Errorf("object is not Error. got=%T (%+v)", evaluted, evaluted)
+				t.Errorf("object is not Error. got=%T (%+v)", evaluated, evaluated)
 				continue
 			}
 			if errObj.Message != expected {
-				t.Errorf("Wrong error message. expected=%q, got=%q", expected, errObj.Message)
+				t.Errorf("wrong error message. expected=%q, got=%q", expected, errObj.Message)
+			}
+		case []int:
+			array, ok := evaluated.(*object.Array)
+			if !ok {
+				t.Errorf("obj not Array. got=%T (%+v)", evaluated, evaluated)
+				continue
+			}
+
+			if len(array.Elements) != len(expected) {
+				t.Errorf("wrong num of elements. want=%d, got=%d",
+					len(expected), len(array.Elements))
+				continue
+			}
+
+			for i, expectedElem := range expected {
+				testIntegerObject(t, array.Elements[i], int64(expectedElem))
 			}
 		}
 	}
--- repl/repl.go
+++ repl/repl.go
@@ -40,22 +40,20 @@ func Start(in io.Reader, out io.Writer) {
 	}
 }
 
-const MONKEY_FACE = `            __,__
-   .--.  .-"     "-.  .--.
-  / .. \/  .-. .-.  \/ .. \
- | |  '|  /   Y   \  |'  | |
- | \   \  \ 0 | 0 /  /   / |
-  \ '- ,\.-"""""""-./, -' /
-   ''-' /_   ^ ^   _\ '-''
-       |  \._   _./  |
-       \   \ '~' /   /
-        '._ '-=-' _.'
-           '-----'
+const WAFFLE = `            
+                                  ad88    ad88 88             
+                                d8"     d8"   88             
+                                88      88    88             
+8b      db      d8 ,adPPYYba, MM88MMM MM88MMM 88  ,adPPYba,
+ 8b    d88b    d8' ""      Y8   88      88    88 a8P_____88  
+  8b  d8' 8b  d8'  ,adPPPPP88   88      88    88 8PP""""""" 
+   8bd8'   8bd8'   88,    ,88   88      88    88 "8b,   ,aa
+    YP      YP      "8bbdP"Y8   88      88    88   "Ybbd8"'
 `
 
 func printParserErrors(out io.Writer, errors []string) {
-	io.WriteString(out, MONKEY_FACE)
-	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, WAFFLE)
+	io.WriteString(out, "Woops! We ran into some sticky business here!\n\n")
 	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
